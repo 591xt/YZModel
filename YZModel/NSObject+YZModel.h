5d31a004fa4cfc1e0b72836ecb3d3021df4f4c76
yz <591xt@591xt.xyz>
2020-04-19 21:45:14 +0800

    YZ

--- YZModel/NSObject+YZModel.h
+++ YZModel/NSObject+YZModel.h
@@ -1,12 +1,28 @@
 
+
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface NSObject (Model)
 
-+(instancetype)withModel:(NSDictionary*)dic;
-+(instancetype)withArrayModel:(NSArray*)array;
+
+/// 字典转模型
+/// @param dic 字典
++(instancetype)withDic:(NSDictionary*)dic;
+
+/// 字典数组转模型数组
+/// @param dicArray 字典数组
++(instancetype)withDicArray:(NSArray*)dicArray;
+
+/// 模型转字典
+/// @param model 模型
+-(NSDictionary*)withModel:(id)model;
+
+/// 模型数组转字典数组
+/// @param modelArray 模型数组
+-(NSArray*)withModelArray:(NSArray*)modelArray;
+
 -(NSDictionary*)arrayModel;
 
 @end
