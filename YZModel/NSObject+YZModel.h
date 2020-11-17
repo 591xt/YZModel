783a823764265eefe086372dbf6413a8a9af23be
yz <591xt@591xt.xyz>
2020-11-17 21:39:53 +0800

    yz

--- YZModel/NSObject+YZModel.h
+++ YZModel/NSObject+YZModel.h
@@ -9,17 +9,17 @@ NS_ASSUME_NONNULL_BEGIN
 
 /// 字典转模型
 /// @param dic 字典
-+(instancetype)withDic:(id)dic;
++(instancetype)yzModelwithDic:(id)dic;
 
 /// 字典数组转模型数组
 /// @param dicArray 字典数组
-+(instancetype)withDicArray:(id)dicArray;
++(instancetype)yzModelArraywithDicArray:(id)dicArray;
 
 /// 模型转字典
--(NSDictionary*)withModel;
+-(NSDictionary*)yzDicwithModel;
 
 /// 模型数组转字典数组
--(NSArray*)withModelArray;
+-(NSArray*)yzDicwithModelArray;
 
 -(NSDictionary*)arrayModel;
 
